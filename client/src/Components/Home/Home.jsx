37a57c5df527ada413ff198671db17ddf5d07324
rashedsabbir <rashedulhasan1996@gmail.com>
2023-06-14 15:22:53 +0600

    vite added

---
+++ client/src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">
+        DaisyUI working
+      </button>
+    </div>
+  );
+};
+
+export default Home;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,9 +4,9 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: ["emerald"],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 
